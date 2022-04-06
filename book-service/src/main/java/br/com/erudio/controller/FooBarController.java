769497d20a16722aca089d951e88a816c182fc3e
5hapeshifter <suleimandiverio@gmail.com>
2022-04-06 15:31:55 -0300

    configurando o rate limiter bulkhead

--- book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -1,13 +1,11 @@
 package br.com.erudio.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("book-service")
@@ -17,14 +15,16 @@ public class FooBarController {
 
     // fallbackMethod e o metodo que sera chamado para responder a situacao quando ocorrer um erro e esgotar a tentativa de retry
     //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // Configuracao de tentativas de request que deve ser feita configurada no application.yml, tambem podemos implementar essa configuracao por endpoint
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod") // Quando ocorre muitas falhas de requisicoes, o circuit breaker envia as requisicoes direto para o fallback method para a aplicacao nao colapsar
+    //@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod") // Quando ocorre muitas falhas de requisicoes, o circuit breaker envia as requisicoes direto para o fallback method para a aplicacao nao colapsar
+    //@RateLimiter(name = "default") // Determina a quantidade de chamadas que podemos fazer para um ou varios endpoints dentro de um determinado tempo
+    @Bulkhead(name = "default")
     @GetMapping("/foo-bar")
     public String foobar(){
         logger.info("Request to foo-bar is received!");
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar", String.class); // endereco errado propositalmente para gerar erro
-        //return "Foo-Bar!!";
-        return response.getBody();
+//        var response = new RestTemplate() // Usar com o Retry e Circuit Breaker
+//                .getForEntity("http://localhost:8080/foo-bar", String.class); // endereco errado propositalmente para gerar erro
+        return "Foo-Bar!!!";
+        //return response.getBody();
     }
 
     public String fallbackMethod(Exception ex) { // Metodo que sera executado e sera chamado pelo retry do metodo acima, foobar
