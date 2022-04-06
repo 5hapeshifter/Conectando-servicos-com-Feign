745e61d341c539efeb75c1f9db7d3cbf3506ba72
5hapeshifter <suleimandiverio@gmail.com>
2022-04-06 15:17:27 -0300

    configurando o circuit breaker

--- book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -1,5 +1,6 @@
 package br.com.erudio.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,8 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     // fallbackMethod e o metodo que sera chamado para responder a situacao quando ocorrer um erro e esgotar a tentativa de retry
-    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // Configuracao de tentativas de request que deve ser feita configurada no application.yml, tambem podemos implementar essa configuracao por endpoint
+    //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // Configuracao de tentativas de request que deve ser feita configurada no application.yml, tambem podemos implementar essa configuracao por endpoint
+    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod") // Quando ocorre muitas falhas de requisicoes, o circuit breaker envia as requisicoes direto para o fallback method para a aplicacao nao colapsar
     @GetMapping("/foo-bar")
     public String foobar(){
         logger.info("Request to foo-bar is received!");
